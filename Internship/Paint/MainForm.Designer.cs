819c9feb1ee2a2175f7c8ede404a22ddb3f8a1d3
Mihaela Ivanova <miaivanova300@gmail.com>
2021-08-19 14:14:52 +0300

    Initial commit

---
+++ Internship/Paint/MainForm.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace Paint
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainPanel = new System.Windows.Forms.Panel();
+            this.menu = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveBinaryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadBinaryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mainPanel.SuspendLayout();
+            this.menu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mainPanel
+            // 
+            this.mainPanel.Anchor = ((System.Windows.Forms.AnchorStyles) ((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
+            this.mainPanel.BackColor = System.Drawing.Color.Black;
+            this.mainPanel.Controls.Add(this.menu);
+            this.mainPanel.Location = new System.Drawing.Point(0, 0);
+            this.mainPanel.Name = "mainPanel";
+            this.mainPanel.Size = new System.Drawing.Size(1920, 1080);
+            this.mainPanel.TabIndex = 0;
+            this.mainPanel.Click += new System.EventHandler(this.mainPanel_Click);
+            this.mainPanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.mainPanel_MouseDown);
+            // 
+            // menu
+            // 
+            this.menu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {this.fileToolStripMenuItem});
+            this.menu.Location = new System.Drawing.Point(0, 0);
+            this.menu.Name = "menu";
+            this.menu.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.menu.Size = new System.Drawing.Size(1920, 24);
+            this.menu.TabIndex = 0;
+            this.menu.Text = "menu";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {this.saveToolStripMenuItem, this.saveBinaryToolStripMenuItem, this.loadToolStripMenuItem, this.loadBinaryToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(136, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            // 
+            // saveBinaryToolStripMenuItem
+            // 
+            this.saveBinaryToolStripMenuItem.Name = "saveBinaryToolStripMenuItem";
+            this.saveBinaryToolStripMenuItem.Size = new System.Drawing.Size(136, 22);
+            this.saveBinaryToolStripMenuItem.Text = "Save binary";
+            // 
+            // loadToolStripMenuItem
+            // 
+            this.loadToolStripMenuItem.Name = "loadToolStripMenuItem";
+            this.loadToolStripMenuItem.Size = new System.Drawing.Size(136, 22);
+            this.loadToolStripMenuItem.Text = "Load";
+            // 
+            // loadBinaryToolStripMenuItem
+            // 
+            this.loadBinaryToolStripMenuItem.Name = "loadBinaryToolStripMenuItem";
+            this.loadBinaryToolStripMenuItem.Size = new System.Drawing.Size(136, 22);
+            this.loadBinaryToolStripMenuItem.Text = "Load binary";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.mainPanel);
+            this.MainMenuStrip = this.menu;
+            this.Name = "MainForm";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.Text = "Paint";
+            this.mainPanel.ResumeLayout(false);
+            this.mainPanel.PerformLayout();
+            this.menu.ResumeLayout(false);
+            this.menu.PerformLayout();
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.MenuStrip menu;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveBinaryToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadBinaryToolStripMenuItem;
+
+
+        private System.Windows.Forms.Panel mainPanel;
+
+        #endregion
+    }
+}
---
+++ Internship/Paint/MainForm.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Paint
+{
+    public partial class MainForm : Form
+    {
+        private readonly Graphics _graphics;
+        private Point _mouseLocation;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            _graphics = mainPanel.CreateGraphics();
+        }
+
+        private void mainPanel_Click(object sender, EventArgs e)
+        {
+            var startingPoint = GetRandomStartingPoint(_mouseLocation);
+            var x2 = 2 * _mouseLocation.X - startingPoint.X;
+            var y2 = 2 * _mouseLocation.Y - startingPoint.Y;
+            
+            var width = x2 - startingPoint.X;
+            var height = y2 - startingPoint.Y;
+            
+            var size = new Size(width, height);
+            var rect = new Rectangle(startingPoint, size);
+            
+            
+            _graphics.DrawRectangle(new Pen(Color.White, 1.0f), rect);
+        }
+        
+        private static Point GetRandomStartingPoint(Point middle)
+        {
+            var random = new Random();
+            var x1 = random.Next(middle.X - 100, middle.X - 50);
+            var y1 = random.Next(middle.Y - 100, middle.Y - 50);
+            var start = new Point(x1, y1);
+            return start;
+        }
+
+        private void mainPanel_MouseDown(object sender, MouseEventArgs e)
+        {
+            _mouseLocation = e.Location;
+        }
+    }
+}
